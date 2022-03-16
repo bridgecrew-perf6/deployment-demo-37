cdb6adf68115341ef806523d69f24e921b168d0c
Isarel Getu <israelgetu101@gmail.com>
2022-03-16 13:20:12 -0500

    css added

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.get("/", function (req, res) {
 });
 
 app.get("/styles", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.css"));
 });
 
 const port = process.env.PORT || 4005;
